02daf59189dd773da3a0178bfdda44dc5b55b52d
Jiehan <jiehan.develop@gmail.com>
2020-02-05 17:28:26 +0800

    added validation

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,135 +1,17 @@
 function adminController(models) {
   async function register(req, res) {
-    let teacherEmail = req.body.teacher;
-    let studentEmails = req.body.students;
+    try {
+      const teacherEmail = req.body.teacher;
+      const studentEmails = req.body.students;
 
-    if (!teacherEmail) {
-      return res.status(500).json({ message: 'Please provide a valid teacher email' });
-    }
-
-    if (!studentEmails) {
-      return res.status(500).json({ message: 'Please provide atleast one valid student email' });
-    }
-
-    teacherEmail = teacherEmail.trim().toLowerCase();
-    studentEmails = studentEmails.map((e) => e.trim().toLowerCase());
-
-    // Create teacher if not exist
-    const [teacher] = await models.Teacher.findOrCreate({
-      where: { email: teacherEmail }
-    });
-
-    // Create students if not exist
-    const students = await Promise.all(
-      studentEmails.map(async (e) => {
-        const [student] = await models.Student.findOrCreate({ where: { email: e } });
-
-        return student;
-      })
-    );
-
-    teacher.addStudents(students);
-
-    return res.sendStatus(204);
-  }
-
-  async function suspend(req, res) {
-    let studentEmail = req.body.student;
-
-    if (!studentEmail) {
-      res.status(400).json({ message: 'Please provide a valid student email' });
-      return;
-    }
-
-    studentEmail = studentEmail.trim().toLowerCase();
-
-    const student = await models.Student.findOne({
-      where: { email: studentEmail }
-    });
-
-    if (!student) {
-      res.status(404).json({ message: `Specified student '${studentEmail}' not found` });
-      return;
-    }
-
-    if (!student.suspend) {
-      await student.update({
-        suspend: true
+      // Create teacher if not exist
+      const [teacher] = await models.Teacher.findOrCreate({
+        where: { email: teacherEmail.trim().toLowerCase() }
       });
-    }
 
-    res.sendStatus(204);
-  }
-
-  async function commonStudents(req, res) {
-    if (!req.query.teacher) {
-      res.status(200).json({ students: [] });
-      return;
-    }
-
-    const teacherEmails = Array.isArray(req.query.teacher)
-      ? req.query.teacher
-      : [req.query.teacher];
-
-    const teachers = await Promise.all(
-      teacherEmails.map(async (e) => {
-        const teacher = await models.Teacher.findOne({
-          where: { email: e },
-          include: models.Student
-        });
-        return { email: e, teacher };
-      })
-    );
-
-    const teacherNotFoundMsg = teachers
-      .filter((t) => t.teacher == null)
-      .map((t) => `Specified teacher '${t.email} not found'`)
-      .join(', ');
-
-    if (teacherNotFoundMsg) {
-      res.status(404).json({ message: teacherNotFoundMsg });
-      return;
-    }
-
-    const studentEmailsList = teachers
-      .filter((t) => t.teacher.students != null && t.teacher.students.length > 0)
-      .map((t) => t.teacher.students.map((s) => s.email));
-
-    let result = studentEmailsList[0];
-
-    for (let i = 1; i < studentEmailsList.length; i += 1) {
-      result = result.filter((x) => studentEmailsList[i].indexOf(x) !== -1);
-    }
-
-    res.status(200).json({ students: result });
-  }
-
-  async function retrieveForNotifications(req, res) {
-    const { teacher: teacherEmail, notification } = req.body;
-
-    if (!teacherEmail) {
-      res.status(400).json({ message: 'Please provide a valid teacher email' });
-      return;
-    }
-
-    const [teacher] = await models.Teacher.findOrCreate({
-      where: { email: teacherEmail.trim().toLowerCase() },
-      include: models.Student
-    });
-
-    if (!teacher) {
-      res.status(404).json({ message: `Specified teacher '${teacherEmail} not found` });
-      return;
-    }
-
-    let registeredStudents = teacher.students.filter((s) => !s.suspend).map((s) => s.email);
-
-    const re = /(?<=^@|\s@)(\w+@[^\s@]+)(?=\s|$)/g;
-    const taggedStudentEmails = notification.match(re);
-
-    if (taggedStudentEmails) {
-      const taggedStudents = await Promise.all(
-        taggedStudentEmails.map(async (e) => {
+      // Create students if not exist
+      const students = await Promise.all(
+        studentEmails.map(async (e) => {
           const [student] = await models.Student.findOrCreate({
             where: { email: e.trim().toLowerCase() }
           });
@@ -138,12 +20,124 @@ function adminController(models) {
         })
       );
 
-      const validTaggedStudentEmails = taggedStudents.filter((s) => !s.suspend).map((s) => s.email);
+      teacher.addStudents(students);
 
-      registeredStudents = Array.from(new Set(registeredStudents.concat(validTaggedStudentEmails)));
+      res.sendStatus(204);
+    } catch (err) {
+      res.status(500).json({ message: err.message });
     }
+  }
 
-    res.status(200).json({ recipients: registeredStudents });
+  async function suspend(req, res) {
+    try {
+      const studentEmail = req.body.student;
+
+      const student = await models.Student.findOne({
+        where: { email: studentEmail.trim().toLowerCase() }
+      });
+
+      if (!student) {
+        res.status(404).json({ message: `Specified student '${studentEmail}' not found` });
+        return;
+      }
+
+      if (!student.suspend) {
+        await student.update({
+          suspend: true
+        });
+      }
+
+      res.sendStatus(204);
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  }
+
+  async function commonStudents(req, res) {
+    try {
+      const teacherEmails = Array.isArray(req.query.teacher)
+        ? req.query.teacher
+        : [req.query.teacher];
+
+      const teachers = await Promise.all(
+        teacherEmails.map(async (e) => {
+          const teacher = await models.Teacher.findOne({
+            where: { email: e },
+            include: models.Student
+          });
+          return { email: e, teacher };
+        })
+      );
+
+      const teacherNotFoundMsg = teachers
+        .filter((t) => t.teacher == null)
+        .map((t) => `Specified teacher '${t.email} not found'`)
+        .join(', ');
+
+      if (teacherNotFoundMsg) {
+        res.status(404).json({ message: teacherNotFoundMsg });
+        return;
+      }
+
+      const studentEmailsList = teachers
+        .filter((t) => t.teacher.students != null && t.teacher.students.length > 0)
+        .map((t) => t.teacher.students.map((s) => s.email));
+
+      let result = studentEmailsList[0];
+
+      for (let i = 1; i < studentEmailsList.length; i += 1) {
+        result = result.filter((x) => studentEmailsList[i].indexOf(x) !== -1);
+      }
+
+      res.status(200).json({ students: result });
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  }
+
+  async function retrieveForNotifications(req, res) {
+    try {
+      const { teacher: teacherEmail, notification } = req.body;
+
+      const [teacher] = await models.Teacher.findOrCreate({
+        where: { email: teacherEmail.trim().toLowerCase() },
+        include: models.Student
+      });
+
+      if (!teacher) {
+        res.status(404).json({ message: `Specified teacher '${teacherEmail} not found` });
+        return;
+      }
+
+      let registeredStudents = teacher.students.filter((s) => !s.suspend).map((s) => s.email);
+
+      const re = /(?<=^@|\s@)(\w+@[^\s@]+)(?=\s|$)/g;
+      const taggedStudentEmails = notification.match(re);
+
+      if (taggedStudentEmails) {
+        const taggedStudents = await Promise.all(
+          taggedStudentEmails.map(async (e) => {
+            const [student] = await models.Student.findOrCreate({
+              where: { email: e.trim().toLowerCase() }
+            });
+
+            return student;
+          })
+        );
+
+        const validTaggedStudentEmails = taggedStudents
+          .filter((s) => !s.suspend)
+          .map((s) => s.email);
+
+        registeredStudents = Array.from(
+          new Set(registeredStudents.concat(validTaggedStudentEmails))
+        );
+      }
+
+      res.status(200).json({ recipients: registeredStudents });
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
   }
 
   return {
--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { check, validationResult } = require('express-validator');
 
 const Teacher = require('../database/models/Teacher');
 const Student = require('../database/models/Student');
@@ -9,12 +10,68 @@ const controller = adminController({ Teacher, Student, TeacherStudentRegistry })
 
 const router = express.Router();
 
-router.post('/register', controller.register);
+const validate = (validations) => async (req, res, next) => {
+  await Promise.all(validations.map((validation) => validation.run(req)));
 
-router.get('/commonstudents', controller.commonStudents);
+  const result = validationResult(req);
 
-router.post('/suspend', controller.suspend);
+  if (result.isEmpty()) {
+    return next();
+  }
 
-router.post('/retrievefornotifications', controller.retrieveForNotifications);
+  const errorMsg = result.errors.map((e) => `The field for '${e.param}' ${e.msg}.`).join(' ');
+
+  return res.status(400).json({ message: errorMsg });
+};
+
+const registerValidation = [
+  check('teacher')
+    .notEmpty()
+    .withMessage('cannot be null or empty')
+    .isEmail()
+    .withMessage('must be a valid email'),
+  check('students')
+    .isArray()
+    .withMessage('must be of type array')
+    .notEmpty()
+    .withMessage('cannot be null or empty')
+];
+
+const suspendValidation = [
+  check('student')
+    .notEmpty()
+    .withMessage('cannot be null or empty')
+    .isEmail()
+    .withMessage('must be a valid email')
+];
+
+const commonStudentsValidation = [
+  check('teacher')
+    .notEmpty()
+    .withMessage('cannot be null or empty')
+];
+
+const retrieveForNotificationsValidation = [
+  check('teacher')
+    .notEmpty()
+    .withMessage('cannot be null or empty')
+    .isEmail()
+    .withMessage('must be a valid email'),
+  check('notification')
+    .notEmpty()
+    .withMessage('cannot be null or empty')
+];
+
+router.post('/register', validate(registerValidation), controller.register);
+
+router.get('/commonstudents', validate(commonStudentsValidation), controller.commonStudents);
+
+router.post('/suspend', validate(suspendValidation), controller.suspend);
+
+router.post(
+  '/retrievefornotifications',
+  validate(retrieveForNotificationsValidation),
+  controller.retrieveForNotifications
+);
 
 module.exports = router;
